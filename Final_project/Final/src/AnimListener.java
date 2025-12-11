464564b3c578a9b88ca23cf9f055cef061c00bfb
Nabil Nasser <82504675+Nabil-Nasser@users.noreply.github.com>
2025-12-11 23:15:55 +0200

    Add files via upload

---
+++ Final_project/Final/src/AnimListener.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.media.opengl.GLEventListener;
+import java.awt.event.KeyListener;
+
+/**
+ *
+ * @author Tyba
+ */
+
+public abstract class AnimListener implements GLEventListener, KeyListener {
+ 
+    protected String assetsFolderName = "Assets";
+    
+}
